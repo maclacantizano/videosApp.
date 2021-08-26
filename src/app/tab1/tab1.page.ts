c4e0f81d18998d469203b1c53797c482f30eadfa
Maria Clara Macedo Cantizano <maria.cantizano@entec.sp.gov.br>
2021-08-26 18:07:13 -0300

    declarei, utilizando variáveis e criei interfaces

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
+import { IFilme } from '../models/IFilme.model';
 
 
 @Component({
@@ -10,7 +11,58 @@ import { ToastController } from '@ionic/angular';
 })
 export class Tab1Page {
 
-  constructor(public alertController: AlertController, public toastController: ToastController) {}
+  titulo = "Vídeos App";
+
+  listaVideos: IFilme[] = [
+    {
+      nome: 'Viúva Negra (2021)',
+      lancamento: '08/07/2021',
+      duracao: '2h 13m',
+      classificacao: 76,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/rKq1Vlw0Bqe2EEvdmIkkkgPQAGf.jpg',
+      generos: ['Ação', 'Aventura', 'Ficção científica']
+    },
+
+    {
+      nome:'Mulher Maravilha (2020)',
+      lancamento:'17/12/2020',
+      duracao: '2h 31m',
+      classificacao: 67,
+      cartaz:'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/AweDlIiohqw4sjC7gCsPVd1zo77.jpg',
+      generos: ['Ação', 'Aventura', 'Fantasia']
+    },
+
+    {
+      nome:'Din e o Dragão genial (2021)',
+      lancamento:'15/01/2021',
+      duracao: '1h 38m',
+      classificacao: 83,
+      cartaz:'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/hNhGNWn4Z9S77cOuYn2ZWfC9E7b.jpg',
+      generos: ['Animação', 'Família', 'Comédia', 'Fantasia']
+    },
+
+    {
+      nome:'Hotel Transilvânia (2012)',
+      lancamento:'05/10/2012',
+      duracao: '1h 31m',
+      classificacao: 69,
+      cartaz:'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/i9kAWShk7qrqril2dLboP0hi8I2.jpg',
+      generos: ['Animação', 'Comédia', 'Família', 'Fantasia']
+    },
+
+    {
+      nome:'Crepúsculo Amanhecer 2 (2012)',
+      lancamento:'15/11/2012',
+      duracao: ' 1h 55m',
+      classificacao: 65,
+      cartaz:'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/pVNobbWwSM2xpv2rRejxwnHf4xH.jpg',
+      generos: ['Aventura', 'Fantasia', 'Drama', 'Romance']
+    },
+
+
+  ];
+
+  constructor(public alertController: AlertController, public toastController: ToastController) { }
 
   async exibirAlertaFavorito() {
     const alert = await this.alertController.create({
